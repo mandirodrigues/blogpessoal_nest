b8b6678d30d3ddbcfe9579beedfc68aa7bdba8f8
Amanda <amandac@genstudents.org>
2025-08-06 09:17:35 -0300

    cadastro de usuarios

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -1,11 +1,12 @@
-import { IsNotEmpty } from "class-validator";
-import { Tema } from "src/tema/entities/tema.entity";
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { IsNotEmpty } from "class-validator"
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
+import { Tema } from "../../tema/entities/tema.entity"
+import { Usuario } from "../../usuario/entities/usuario.entity"
 
 @Entity({name: "tb_postagens"})
 export class Postagem {
 
-    @PrimaryGeneratedColumn()
+    @PrimaryGeneratedColumn()    
     id: number
 
     @IsNotEmpty()
@@ -18,10 +19,15 @@ export class Postagem {
 
     @UpdateDateColumn()
     data: Date
-
+    
     @ManyToOne(() => Tema, (tema) => tema.postagem, {
         onDelete: "CASCADE"
     })
     tema: Tema
-    
+
+    @ManyToOne(() => Usuario, (usuario) => usuario.postagem, {
+        onDelete: "CASCADE"
+    })
+    usuario: Usuario
+
 }
