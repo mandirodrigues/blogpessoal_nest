323e4ba397e1537dc9fc7d9d28388e56db344b97
Amanda <amandac@genstudents.org>
2025-08-08 11:31:35 -0300

    Adiciona e configura os testes com Jest.

--- src/postagem/postagem.module.ts
+++ src/postagem/postagem.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { PostagemController } from "./controllers/postagem.controller";
 import { Postagem } from "./entities/postagem.entity";
 import { PostagemService } from "./services/postagem.service";
-import { TemaModule } from "src/tema/tema.module";
+import { TemaModule } from "../tema/tema.module";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Postagem]), TemaModule],
