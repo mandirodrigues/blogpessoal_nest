1333fb8b4e5a9a7402a73660792e87a873d00cb4
Amanda <amandac@genstudents.org>
2025-08-14 22:14:30 -0300

    configuracao do render

--- src/tema/entities/tema.entity.ts
+++ src/tema/entities/tema.entity.ts
@@ -1,17 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty } from "class-validator";
-import { Postagem } from '../../postagem/entities/postagem.entity';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Postagem } from "../../postagem/entities/postagem.entity";
 
 @Entity({name: "tb_temas"})
 export class Tema {
 
-    @PrimaryGeneratedColumn()    
+    @PrimaryGeneratedColumn()  
+    @ApiProperty()   
     id: number
 
     @IsNotEmpty()
     @Column({length: 255, nullable: false})
+    @ApiProperty() 
     descricao: string
 
+    @ApiProperty() 
     @OneToMany(() => Postagem, (postagem) => postagem.tema)
     postagem: Postagem[]
     
